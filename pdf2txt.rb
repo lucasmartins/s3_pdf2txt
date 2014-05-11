bc7427c5ba49c9769c0d18aecb329d3c12ec85c4
Lucas N Martins <lucasmartins@railsnapraia.com>
2014-05-11 13:34:29 -0300

    refining endpoint

--- pdf2txt.rb
+++ pdf2txt.rb
@@ -1,19 +1,32 @@
+require 'json'
+
 class Pdf2txt < Sinatra::Base
   post '/convert' do
     content_type :json
-    unless Sinatra::Hat.validate_params(params,['file_url','callback_url'])
-      status 400
-      json({'message'=>'invalid params'})
-    else
-      status 200
+    begin
+      params = JSON.parse(request.body.read)  
+      unless Sinatra::Hat.validate_params(params,['file_url','callback_url'])
+        invalid_params
+      else
+        jid = ConverterWorker.perform_async(params['file_url'],params['callback_url'])
+        status [200,{jid: jid}]
+      end
+    rescue JSON::ParserError => e
+      invalid_params
     end
-    # convert pdf
-    # call callback telling the url of the TXT file.
   end
+
   if ENV['RACK_ENV']=='development'
     post '/mock_callback' do
       content_type :json
+      puts request.inspect
       status 200
     end  
+  end
+
+  private
+  def invalid_params
+    status 400
+    json({'message'=>'invalid params'})
   end
 end
--- spec/pdf2txt_spec.rb
+++ spec/pdf2txt_spec.rb
@@ -6,15 +6,17 @@ def app
 end
 
 describe Pdf2txt do
+  let(:params){ {'file_url'=>'http://s3.amazon.com/foo/bar.pdf', 'callback_url'=>'http://0.0.0.0/txt/callback'} }
   context 'with good params' do
     it 'schedules the job' do
-      post '/convert', {'file_url'=>'http://s3.amazon.com/foo/bar.pdf', 'callback_url'=>'http://0.0.0.0/txt/callback'}
+      ConverterWorker.should_receive(:perform_async).with(params['file_url'],params['callback_url'])
+      post '/convert', params.to_json, {content_type: 'application/json'}
       expect(last_response.status).to eq(200)
     end
   end
   context 'with bad params' do
     it "raises params error" do
-      post '/convert'
+      post '/convert', '', {content_type: 'application/json'}
       expect(JSON.parse(last_response.body)).to eq({"message"=>"invalid params"})
       expect(last_response.status).to eq(400)
     end
