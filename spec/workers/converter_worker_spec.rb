9b3fdafb4818bc3048eb00c38fe874cec94a124b
Lucas N Martins <lucasmartins@railsnapraia.com>
2014-05-12 18:29:47 -0300

    added gvt test

--- spec/workers/converter_worker_spec.rb
+++ spec/workers/converter_worker_spec.rb
@@ -3,22 +3,44 @@ require "spec_helper"
 describe ConverterWorker do
 
   let(:file_url) { 'https://s3.amazonaws.com/teleforce-test/stub.pdf' }
+  let(:file_url_2) { 'https://s3.amazonaws.com/teleforce-test/neogrid_gvt.pdf' }
   let(:storage_path) { PdfConverter.storage_path(file_url.split('/').last) }
   let(:callback_url) { 'https://0.0.0.0/txt/callback' }
   
-  describe '#perform' do
-    def run_job
-      Sidekiq::Testing.inline! do
-        ConverterWorker.perform_async(file_url,callback_url)
+  context 'vivo' do
+    describe '#perform' do
+      def run_job
+        Sidekiq::Testing.inline! do
+          ConverterWorker.perform_async(file_url,callback_url)
+        end
+      end
+      it 'retrieves the file successfully' do
+        run_job
+        expect(File.exists?(storage_path)).to be_true
+      end
+      it "calls callback" do
+        ServiceCallback.should_receive(:send).and_return('something')
+        run_job
       end
     end
-    it 'retrieves the file successfully' do
-      run_job
-      expect(File.exists?(storage_path)).to be_true
-    end
-    it "calls callback" do
-      ServiceCallback.should_receive(:send).and_return('something')
-      run_job
+  end
+
+  context 'gvt' do
+    describe '#perform' do
+      def run_job
+        Sidekiq::Testing.inline! do
+          ConverterWorker.perform_async(file_url_2,callback_url)
+        end
+      end
+      it 'retrieves the file successfully' do
+        run_job
+        expect(File.exists?(storage_path)).to be_true
+      end
+      it "calls callback" do
+        ServiceCallback.should_receive(:send).and_return('something')
+        run_job
+      end
     end
   end
+  
 end
