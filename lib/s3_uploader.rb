8304d69126fe7aebd30f89b791a829191678d7a0
Lucas N Martins <lucasmartins@railsnapraia.com>
2014-05-15 15:47:00 -0300

    ugly workaround

--- lib/s3_uploader.rb
+++ lib/s3_uploader.rb
@@ -2,6 +2,7 @@ class S3Uploader
   include AWS::S3
   def self.upload(file_origin,file_destination, bucket=ENV['AWS_S3_BUCKET'], &block)
     raise "Invalid destination: #{file_destination}" if file_destination =~ /http/
+    file_destination.gsub!('%2F','/') if file_destination.include?('%2F') # ugly workaround
     AWS::S3::Base.establish_connection!(access_key_id: ENV['AWS_S3_KEY'], secret_access_key: ENV['AWS_S3_SECRET'])
     response = S3Object.store(file_destination, open(file_origin), bucket, :access => :public_read)
     raise "Couldn't upload file!" unless response.code==200
