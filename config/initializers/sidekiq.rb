c181432bbcac4ee22b6daf966318b5c9d0f90c51
Lucas N Martins <lucasmartins@railsnapraia.com>
2014-05-22 16:27:04 -0300

    using heroku env names for Redis

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,8 @@
+# rewrite for heroku sake
+if ENV['REDISTOGO_URL']
+  ENV['REDIS_URL']=ENV['REDISTOGO_URL']
+end
+
 Sidekiq.configure_server do |config|
   config.redis = { :url => ENV['REDIS_URL'], :namespace => 'pdf2txt' }
   environment = ENV['RAILS_ENV'] || ENV['RACK_ENV'] || 'development'
