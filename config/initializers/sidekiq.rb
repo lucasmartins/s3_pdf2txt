1f3ed9be0eaf1cffd3c47829b80e6fe496d6f623
Lucas N Martins <lucasmartins@railsnapraia.com>
2014-08-12 17:55:27 -0300

    setting concurrency from env

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,6 +3,9 @@ if ENV['REDISTOGO_URL']
   ENV['REDIS_URL']=ENV['REDISTOGO_URL']
 end
 
+Sidekiq.options[:concurrency]=ENV['SIDEKIQ_CONCURRENCY'].to_i if ENV['SIDEKIQ_CONCURRENCY']
+puts Sidekiq.options
+
 Sidekiq.configure_server do |config|
   config.redis = { :url => ENV['REDIS_URL'], :namespace => 'pdf2txt' }
   environment = ENV['RAILS_ENV'] || ENV['RACK_ENV'] || 'development'
