e79b36d5a5bc911a785783b289bfdc470292c0c5
Lucas N Martins <lucasmartins@railsnapraia.com>
2014-08-12 10:04:48 -0300

    using instance for uplaod

--- lib/workers/s3_uploader_worker.rb
+++ lib/workers/s3_uploader_worker.rb
@@ -3,6 +3,6 @@ require 'aws/s3'
 class S3UploaderWorker
   include Sidekiq::Worker
   def perform(file_origin,file_destination)
-    S3Uploader.upload(file_origin, file_destination)
+    S3Uploader.new.upload(file_origin, file_destination)
   end
 end
