8bf756e3629bd46218ce9cad864411cc7c9a75c6
Lucas N Martins <lucasmartins@railsnapraia.com>
2014-08-12 15:17:43 -0300

    @@mutex

--- lib/workers/s3_uploader_worker.rb
+++ lib/workers/s3_uploader_worker.rb
@@ -2,11 +2,8 @@ require 'aws/s3'
 
 class S3UploaderWorker
   include Sidekiq::Worker
-  @@mutex = Mutex.new
 
   def perform(file_origin,file_destination)
-    @@mutex.synchronize do
-      S3Uploader.upload(file_origin, file_destination)
-    end
+    S3Uploader.upload(file_origin, file_destination)
   end
 end
