6c83f4b0ed72a726866164c984d9c494982965f8
Lucas N Martins <lucasmartins@railsnapraia.com>
2014-10-30 10:24:26 -0200

    logging callbacks

--- lib/workers/callback_worker.rb
+++ lib/workers/callback_worker.rb
@@ -1,6 +1,7 @@
 class CallbackWorker
   include Sidekiq::Worker
   def perform(callback_url, body, params)
+    puts "Calling back to #{callback_url} with params: #{params}, and body:\n#{body}"
     ServiceCallback.send(callback_url, body, params)
   end
 end
